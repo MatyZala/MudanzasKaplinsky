fa694f8e292aed16697232b623c084e389839471
AWERMatiaszalazar <zalazarmatiasleonardo@gmail.com>
2023-11-07 12:17:42 -0300

    change instagram & SEO description

--- src/constants/index.js
+++ src/constants/index.js
@@ -85,7 +85,7 @@ export const socialMedia = [
   {
     id: "social-media-1",
     icon: instagram,
-    link: "https://www.instagram.com/mudanzaskaplinsky/",
+    link: "https://www.instagram.com/lamodernamuda/",
   },
   // {
   //   id: "social-media-2",
