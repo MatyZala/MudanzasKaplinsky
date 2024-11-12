833e9bcf54e00f74d0865dc9133808710a4098b1
Gonzalo Ribero Turus <gonzaloribero98@gmail.com>
2024-11-12 08:39:37 -0300

    Change phone number

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,7 @@ const Button = ({ styles }) => {
 
   return (
     <p>
-      <a href="https://api.whatsapp.com/send?phone=+5493518567123&text=Hola!%20Me%20comunico%20desde%20la%20web.%20Quisiera%20realizar%20un%20presupuesto%20para%20una%20mudanza!" target='_blank'>{t('button.start')}</a>
+      <a href="https://api.whatsapp.com/send?phone=+5493512776666&text=Hola!%20Me%20comunico%20desde%20la%20web.%20Quisiera%20realizar%20un%20presupuesto%20para%20una%20mudanza!" target='_blank'>{t('button.start')}</a>
     </p>
   )
 };
--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -9,7 +9,7 @@ const GetStarted = () => {
   return (
       <a 
         className="font-poppins flex text-lg bg-green-600 text-slate-200 hover:bg-slate-200 hover:text-green-600 px-4 py-2 rounded-md gap-2 w-48 h-12 hover:animate-pulse" 
-        href="https://api.whatsapp.com/send?phone=+5493518567123&text=Hola!%20Me%20comunico%20desde%20la%20web.%20Quisiera%20realizar%20un%20presupuesto%20para%20una%20mudanza!" 
+        href="https://api.whatsapp.com/send?phone=+5493512776666&text=Hola!%20Me%20comunico%20desde%20la%20web.%20Quisiera%20realizar%20un%20presupuesto%20para%20una%20mudanza!" 
         target='_blank'>
         <span className="justify-center">{t('button.start')}</span>
         <span className="justify-center"><i class="fa fa-whatsapp"></i></span>
